49073969ab9c86ca00077a9a85418a1ce5b14749
layla  hammoud <laylahammoud63@gmail.com>
2021-01-01 14:21:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  return sourceArray.map(x => x * -1);
+}
+function mapToNoChange(sourceArray){
+  return sourceArray.map(x=>x);
+}
+function mapToDouble(sourceArray){
+  return sourceArray.map(x=>x*2)
+}
+function mapToSquare(sourceArray){
+  return sourceArray.map(x=>x*x)
+
+}
+function reduceToTotal(sourceArray){
+  return sourceArray.reduce((accumulator, currentValue) => accumulator + currentValue)
+}
